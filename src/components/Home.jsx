bfd6be75b8acd60654c7c39824d3cbb017a38321
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2022-10-24 22:47:41 -0400

    minor fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,10 @@ export default function Home() {
                     <Card.Body>
                         <h2 className='text-center mb-4 display-1'>Login</h2>
                         <div className="w-100 text-center mt-5 mb-4 ">
-                            <Link className='text-white' to='/student-login'><Button className='w-100'><p className='display-6 m-0'>Student?</p></Button></Link>
+                            <Link className='text-white' to='/student-login'><Button className='w-100'><p className='display-6 m-0'>Student</p></Button></Link>
                         </div>
                         <div className="w-100 text-center mt-4 ">
-                            <Link className='text-white' to='/teacher-login'><Button className='w-100'><p className='display-6 m-0'>Teacher?</p></Button></Link>
+                            <Link className='text-white' to='/teacher-login'><Button className='w-100'><p className='display-6 m-0'>Teacher</p></Button></Link>
                         </div>
                     </Card.Body>
                 </Card>
