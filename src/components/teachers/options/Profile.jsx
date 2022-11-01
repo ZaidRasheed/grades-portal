d9c76d40c18a2bdf3d06f699c4f9fda2345dc1eb
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2022-10-31 23:14:44 -0400

    bug fix again

--- src/components/teachers/options/Profile.jsx
+++ src/components/teachers/options/Profile.jsx
@@ -4,7 +4,7 @@ import UpdatePassword from '../../UpdatePassword'
 export default function Profile(props) {
     return (
         <div className="w-100 mt-5 mb-5">
-            {props.teacher?.name ? <h1 className='display-6 mb-4'>Hello {props.teacher?.gender.toLowerCase() === 'male' ? 'Mr.' : 'Mrs.'} {props.teacher.name}</h1> : null}
+            {props.teacher?.firstName ? <h1 className='display-6 mb-4'>Hello {props.teacher?.gender.toLowerCase() === 'male' ? 'Mr.' : 'Mrs.'} {props.teacher.firstName} {props.teacher.lastName}</h1> : null}
             <h5 className=' mb-5'>Email: {props.teacher?.email}</h5>
             <UpdatePassword />
         </div>
